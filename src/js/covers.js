d13497e4dff9a151f3800e301fc4d98263fa86c9
evgen_zoidze <1ovva.ungrace@gmail.com>
2025-02-08 09:35:38 +0100

    Animation

--- src/js/covers.js
+++ src/js/covers.js
@@ -1,1 +1,21 @@
-console.log("covers script loaded!");
+function toggleAnimation(elementSelector, className) {
+  const element = document.querySelector(elementSelector);
+  if (!element) return;
+
+  const observer = new IntersectionObserver(
+    entries => {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          element.classList.add(className);
+        } else {
+          element.classList.remove(className);
+        }
+      });
+    },
+    { threshold: 0.001 }
+  );
+
+  observer.observe(element);
+}
+
+toggleAnimation('.covers-list', 'covers-animation');
