4ea5533a3ace2da473ac3a86c3a40d4909bcc140
Panchenko Denis <Pandenisko@gmail.com>
2025-02-09 16:44:10 +0200

    added working slider with styles, section is done

--- src/js/api.js
+++ src/js/api.js
@@ -1,17 +1,20 @@
 // GET
-
-
-export function getData() {
-    return fetch(`https://portfolio-js.b.goit.study/api/reviews`, {
-        method: "GET",
-        headers: {
-            "Content-Type": "application/json",
-        }
-    })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-        return response.json();
+export async function getData() {
+  try {
+      const response = await fetch(`https://portfolio-js.b.goit.study/api/reviews`, {
+          method: "GET",
+          headers: {
+              "Content-Type": "application/json",
+          }
       });
+
+      if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      return await response.json();
+  } catch (error) {
+      console.error("Error fetching data:", error);
+      throw error;
   }
+}
