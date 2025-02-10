d4773844b6c4fcd180acdf4ce988b8d73362e502
Dmytro <d.salyakin@gmail.com>
2025-02-10 12:52:50 +0200

    Resolve logic Skills-Slider

--- src/js/about-me.js
+++ src/js/about-me.js
@@ -2,6 +2,7 @@ import Accordion from 'accordion-js';
 import sprite from '../img/icons/icon-sprite.svg';
 
 import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
 
 document.addEventListener('DOMContentLoaded', function () {
   new Accordion('.about-list', {
@@ -35,6 +36,7 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 const slider = new Swiper('.swiper-skills-container', {
+  modules: [Navigation],
   loop: true,
   oneWayMovement: true,
   edgeSwipeDetection: true,
@@ -59,9 +61,9 @@ const slider = new Swiper('.swiper-skills-container', {
       centeredSlides: true,
     },
     1440: {
+      slidesPerGroup: 1,
       slidesPerView: 6,
+      centeredSlides: false,
     },
   },
 });
-
-console.log('Slider: ', slider);
