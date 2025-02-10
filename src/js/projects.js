715489ff002dc6e73aca54ea82bccefd29b896dd
Panchenko Denis <Pandenisko@gmail.com>
2025-02-10 18:24:57 +0200

    verify error is in projects commit
    yes, error in projects

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,28 +1,28 @@
-// 'use strict';
+'use strict';
 
-// import Swiper from 'swiper';
-// import 'swiper/css';
-// import { Navigation, Keyboard, Mousewheel } from 'swiper/modules';
+import Swiper from 'swiper';
+import 'swiper/css';
+import { Navigation, Keyboard, Mousewheel } from 'swiper/modules';
 
-// const nextButton = document.querySelector('.projects-swiper-next-button');
-// const prevButton = document.querySelector('.projects-swiper-prev-button');
+const nextButton = document.querySelector('.projects-swiper-next-button');
+const prevButton = document.querySelector('.projects-swiper-prev-button');
 
-// const swiper = new Swiper('.swiper-projects-container', {
-//   modules: [Navigation, Keyboard, Mousewheel],
-//   navigation: {
-//     nextEl: nextButton,
-//     prevEl: prevButton,
-//   },
-//   spaceBetween: 100,
-//   slidesPerView: 1,
-//   speed: 800,
-//   keyboard: {
-//     enabled: true,
-//     onlyInViewport: true,
-//   },
-//   mousewheel: {
-//     forceToAxis: true,
-//   },
-//   touchEventsTarget: 'wrapper', 
-//   createElements: true,
-// });
+const swiper = new Swiper('.swiper-projects-container', {
+  modules: [Navigation, Keyboard, Mousewheel],
+  navigation: {
+    nextEl: nextButton,
+    prevEl: prevButton,
+  },
+  spaceBetween: 100,
+  slidesPerView: 1,
+  speed: 800,
+  keyboard: {
+    enabled: true,
+    onlyInViewport: true,
+  },
+  mousewheel: {
+    forceToAxis: true,
+  },
+  touchEventsTarget: 'wrapper', 
+  createElements: true,
+});
