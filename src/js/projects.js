23f4719ce2e5ffeab5c51b71589ee9114ea42eb1
Roman Fedorchuk <Romaitgo@gmail.com>
2025-02-10 08:33:20 +0100

    pr

--- src/js/projects.js
+++ src/js/projects.js
@@ -1,41 +1,28 @@
 'use strict';
-import 'swiper/css';
+
 import Swiper from 'swiper';
+import 'swiper/css';
 import { Navigation, Keyboard, Mousewheel } from 'swiper/modules';
 
-const swiper = new Swiper('.uniqu-swiper-container', {
+const nextButton = document.querySelector('.projects-swiper-next-button');
+const prevButton = document.querySelector('.projects-swiper-prev-button');
+
+const swiper = new Swiper('.swiper-projects-container', {
   modules: [Navigation, Keyboard, Mousewheel],
-  speed: 800,
-  slidesPerView: 1,
-  spaceBetween: 0,
-  loop: false,
-
   navigation: {
-    nextEl: '.unique-swiper-button-next[data-swiper-id="1"]',
-    prevEl: '.unique-swiper-button-prev[data-swiper-id="1"]',
+    nextEl: nextButton,
+    prevEl: prevButton,
   },
-
+  spaceBetween: 100,
+  slidesPerView: 1,
+  speed: 800,
   keyboard: {
     enabled: true,
     onlyInViewport: true,
   },
-
-  mousewheel: true,
-
-  on: {
-    slideChange: function () {
-      const prevButton = document.querySelector(
-        '.unique-swiper-button-prev[data-swiper-id="1"]'
-      );
-      const nextButton = document.querySelector(
-        '.unique-swiper-button-next[data-swiper-id="1"]'
-      );
-
-      prevButton.disabled = swiper.isBeginning;
-      nextButton.disabled = swiper.isEnd;
-
-      prevButton.classList.toggle('swiper-button-disabled', swiper.isBeginning);
-      nextButton.classList.toggle('swiper-button-disabled', swiper.isEnd);
-    },
+  mousewheel: {
+    forceToAxis: true,
   },
+  touchEventsTarget: 'wrapper', 
+  createElements: true,
 });
