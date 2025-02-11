12e1498141634a8759320bb7e1c256a2e0253252
Panchenko Denis <Pandenisko@gmail.com>
2025-02-11 02:36:30 +0200

    feature/header
    added working header, styles, js

--- src/js/header.js
+++ src/js/header.js
@@ -1,1 +1,35 @@
-console.log("Header script loaded!");
+const burgerIcon = document.querySelector(".header-menu-img");
+const headerModal = document.querySelector(".header-modal");
+const closeModal = document.querySelector(".header-menu-close-img");
+const headerOrderButton = document.querySelector(".header-modal-btn-order");
+const modalNav = document.querySelector('.header-modal-menu-list');
+const headerMenu = document.querySelector(".header-menu-btn");
+const headerMenuNav = document.querySelector(".header-menu-list.header-hidden");
+
+burgerIcon.addEventListener("click", () => {
+  headerModal.classList.add("active");
+  document.body.classList.add('overflow-hidden');
+});
+
+closeModal.addEventListener("click", () => {
+  headerModal.classList.remove("active");
+  document.body.classList.remove('overflow-hidden');
+});
+
+headerOrderButton.addEventListener("click", () => {
+  headerModal.classList.remove("active");
+  document.body.classList.remove('overflow-hidden');
+})
+
+
+modalNav.addEventListener('click', (event) => {
+  if (event.target.tagName === 'A') {
+    headerModal.classList.remove("active");
+    document.body.classList.remove('overflow-hidden');
+  }
+});
+
+
+headerMenu.addEventListener('click', () => {
+  headerMenuNav.classList.toggle("header-hidden")
+})
